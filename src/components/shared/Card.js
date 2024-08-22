04439a46102ec5979f000c7b13e31246aa123fb9
MuhammadRukon <muhammad.rukon242@gmail.com>
2024-08-22 23:19:44 +0600

    chore: Image optimized

--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -1,10 +1,17 @@
+import Image from "next/image";
 
 const Card = ({ info }) => {
   console.log(info);
   return (
     <div className="w-[131px] text-center">
       <div className="py-[50px] px-[18px] rounded-lg bg-[#050505] border border-[#141414]">
-      <img className="h-[75px] mx-auto" src={info?.imgURL} alt="card-image" /> 
+        <Image 
+          src={info?.imgURL} 
+          alt="card-image" 
+          height={100}
+          width={75}
+          className="bg-contain h-[75px] mx-auto" 
+        />
       </div>
       <p className="text-center text-lg mt-[14px]">{info?.name}</p>
     </div>
