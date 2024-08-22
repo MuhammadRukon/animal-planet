ecf422f2df31837fac43089ac400ecad5eff7a67
MuhammadRukon <muhammad.rukon242@gmail.com>
2024-08-23 00:24:44 +0600

    remote uri added

--- src/api/index.js
+++ src/api/index.js
@@ -2,26 +2,26 @@ import axios from "axios";
 import axiosIntance from "./axiosInstance";
 
 // export const getCategories = async () => await axiosIntance("/categories");
-export const getCategories = async () => await axios("http://localhost:5000/categories", {
+export const getCategories = async () => await axios("https://antopolic-task-server.vercel.app/categories", {
   withCredentials: true,
 });
 
 // export const createCategories = async (name) =>
 //   await axiosIntance.post("/categories/create", name);
 export const createCategories = async (name) =>
-  await axios.post("http://localhost:5000/categories/create", name, {
+  await axios.post("https://antopolic-task-server.vercel.app/categories/create", name, {
     withCredentials: true,
   });
 
 // export const createAnimal = async (data) =>
 //   await axiosIntance.post("/animals/create", data);
 export const createAnimal = async (data) =>
-  await axios.post("http://localhost:5000/animals/create", data, {
+  await axios.post("https://antopolic-task-server.vercel.app/animals/create", data, {
     withCredentials: true,
   });
 
 // export const getAnimals = async () => await axiosIntance("/animals");
 
-export const getAnimals = async () => await axios("http://localhost:5000/animals", {
+export const getAnimals = async () => await axios("https://antopolic-task-server.vercel.app/animals", {
   withCredentials: true,
 });
