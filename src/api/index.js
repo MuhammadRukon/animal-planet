159583f62e35d8c4001382d7b5bd74a1ebef4364
MuhammadRukon <muhammad.rukon242@gmail.com>
2024-08-23 00:16:23 +0600

    axios instance removed for api call issue

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,27 @@
+import axios from "axios";
 import axiosIntance from "./axiosInstance";
 
-export const getCategories = async () => await axiosIntance("/categories");
+// export const getCategories = async () => await axiosIntance("/categories");
+export const getCategories = async () => await axios("http://localhost:5000/categories", {
+  withCredentials: true,
+});
 
+// export const createCategories = async (name) =>
+//   await axiosIntance.post("/categories/create", name);
 export const createCategories = async (name) =>
-  await axiosIntance.post("/categories/create", name, {
+  await axios.post("http://localhost:5000/categories/create", name, {
     withCredentials: true,
   });
 
+// export const createAnimal = async (data) =>
+//   await axiosIntance.post("/animals/create", data);
 export const createAnimal = async (data) =>
-  await axiosIntance.post("/animals/create", data);
+  await axios.post("http://localhost:5000/animals/create", data, {
+    withCredentials: true,
+  });
 
-export const getAnimals = async () => await axiosIntance("/animals");
+// export const getAnimals = async () => await axiosIntance("/animals");
+
+export const getAnimals = async () => await axios("http://localhost:5000/animals", {
+  withCredentials: true,
+});
